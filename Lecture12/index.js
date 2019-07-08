cfb8a6efbf0c144279c0ca80285ca371c850d32c
Shubham Kaushik <shubhamkaushik.jiit@gmail.com>
2019-07-08 09:45:39 +0530

    Handlebars

---
+++ Lecture12/index.js
@@ -0,0 +1,47 @@
+const app = require('express')();
+const hbs = require('hbs');
+const path = require('path')
+
+app.get('/', (req, res) => {
+  res.render('index.hbs', {
+    name: 'shubham',
+    //showname: true
+    description: 'I explain with words and code.'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    name: 'shubham'
+  })
+})
+
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(path.join(__dirname, '/partial'));
+
+app.listen(3000, function(){
+  console.log("Running on 3000")
+});
+
+
+//{{>navbar name=xx}}
+//{{>navbar obj}}
+// app.get('/about', (req, res) => {
+//   res.render('about.hbs', {
+//     name: 'shubham',
+      // obj: {
+      //   name: kaushik
+      // }
+//   })
+// })
+
+
+
+//{{{}}} for rendering html
+
+// {{#if shownmame}}
+//   Hello {{name}}
+// {{else}}
+//   Hello XYZ
+// {{/if}}
